307b79a6f6fd039ad7628c8f64c330a5667bc389
i-z <IvanZdravkovBG@gmail.com>
2019-07-22 16:12:31 +0300

    Scene Management Done.

---
+++ Sky Net Number/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1)
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        else
+            SceneManager.LoadScene(0);
+    }
+}
